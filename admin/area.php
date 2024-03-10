dbce050df20677b85357debf8679d42497ef3c39
Fyke Lleva <floterina@gmail.com>
2024-03-10 22:41:45 +0800

    update the content

--- admin/area.php
+++ admin/area.php
@@ -11,7 +11,7 @@
     <div class="border p-3 rounded mb-3" style="background: white; height: auto">
         <div class="row">
             <div class="col-6 col-md-6 p-2">
-                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('../assets/img/lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
                     <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
                     <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 1</h1>
                     <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
@@ -19,21 +19,21 @@
 
             </div>
             <div class="col-6 col-md-6 p-2">
-                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('../assets/img/lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
                     <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
                     <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 2</h1>
                     <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
                 </div>
             </div>
             <div class="col-6 col-md-6 p-2">
-                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('../assets/img/lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
                     <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
                     <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 3</h1>
                     <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
                 </div>
             </div>
             <div class="col-6 col-md-6 p-2">
-                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('../assets/img/lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
                     <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
                     <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 4</h1>
                     <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
