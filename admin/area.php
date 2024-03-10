74f66a3a58cbdb5a861417febee004c56b422a2c
Fyke Lleva <floterina@gmail.com>
2024-03-10 22:39:55 +0800

    update the content

---
+++ admin/area.php
@@ -0,0 +1,49 @@
+<html lang="en">
+<?php include 'header.php'?>
+<body>
+<div style="display: flex; flex-direction: row">
+<?php include 'navbar.php'?>
+<div class="bg-light w-100 p-3">
+    <div class="border p-3 rounded mb-3" style="background: white; display: flex; flex-direction: row; justify-content: space-between;">
+        <h4 class="fw-bolder ">Area List</h4>
+        <button class="btn btn-sm btn-success">+ New Area</button>
+    </div>
+    <div class="border p-3 rounded mb-3" style="background: white; height: auto">
+        <div class="row">
+            <div class="col-6 col-md-6 p-2">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                    <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
+                    <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 1</h1>
+                    <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
+                </div>
+
+            </div>
+            <div class="col-6 col-md-6 p-2">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                    <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
+                    <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 2</h1>
+                    <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 p-2">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                    <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
+                    <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 3</h1>
+                    <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 p-2">
+                <div class="border rounded p-3" style="height: 300px; background-image: url('lawn.jpg'); background-size: cover; background-position: center; position: relative; overflow: hidden;">
+                    <div class="rounded" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5); border-radius: 15px;"></div>
+                    <h1 class="fw-bolder mt-5" style="font-size: 60px; color: white; position: relative; z-index: 1;">Lawn 4</h1>
+                    <h5 style="color: white; position: relative; z-index: 1;">100 total of lay dead</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'?>
+</div>
+</body>
+</html> 
+
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,8 +1,281 @@
 <html lang="en">
 <?php include 'header.php'?>
 <body>
+<div style="display: flex; flex-direction: row">
+<?php include 'navbar.php'?>
 
-<h5>Dashboard Page</h5>
+<!-- start inventory-->
 
+<div style="width: 100%" class="content p-3">
+    <div>
+        <div style="background-color: white;" class="rounded border p-3 mb-3 w-100">
+            <div class="row">
+                
+                <div class="col-12 col-md-6 p-2">
+                     <h6 class="fw-bolder">Total Death Sales</h6>
+                     <div class="border bg-light border-success rounded p-2" style="height: 200px;">
+                     <h1 class="fw-bolder text-center text-success mt-4" style="font-size: 80px">1290</h1>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 p-2">
+                     <h6 class="fw-bolder">Date and Time</h6>
+                     <div class="border bg-light border-success rounded p-2" style="height: 200px;">
+                    <div style="display: flex; flex-direction: row; justify-content: center" >
+                       <h1 class="fw-bolder text-center text-success mt-4 realtime-time" style="font-size: 80px">time</h1>
+                       <h1 class="fw-bolder text-center text-success mt-4" style="font-size: 80px">&nbsp|&nbsp</h1>
+                       <h1 class="fw-bolder text-center text-success mt-4 realtime-date" style="font-size: 80px">date</h1>
+                     </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-3 p-2">
+                     <h6 class="fw-bolder">Sold</h6>
+                     <div class="border bg-light border-success rounded p-2" style="height: 200px;">
+                     <h1 class="fw-bolder text-center text-success mt-4" style="font-size: 80px">10</h1>
+                     </div>
+                </div>
+                <div class="col-6 col-md-3 p-2">
+                     <h6 class="fw-bolder">Removed</h6>
+                     <div class="border bg-light border-success rounded p-2" style="height: 200px;">
+                     <h1 class="fw-bolder text-center text-success mt-4" style="font-size: 80px">100</h1>
+                    </div>
+                </div>
+                <div class="col-6 col-md-3 p-2">
+                     <h6 class="fw-bolder">Available</h6>
+                     <div class="border bg-light border-success rounded p-2" style="height: 200px;">
+                     <h1 class="fw-bolder text-center text-success mt-4" style="font-size: 80px">332</h1>
+                    </div>
+                </div>
+                <div class="col-6 col-md-3 p-2">
+                     <h6 class="fw-bolder">Reserved</h6>
+                     <div class="border bg-light border-success rounded p-2" style="height: 200px;">
+                     <h1 class="fw-bolder text-center text-success mt-4" style="font-size: 80px">7</h1>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+        <div style="background-color: white;" class="rounded border p-3 mb-3 w-100">
+            <div class="row">
+                <div class="col-12 col-md-6 p-2">
+                     <h6 class="fw-bolder">List of Available</h6>
+                     <div class="border rounded p-2">
+                            <table class="table mt-3">
+                                <thead>
+                                    <tr>
+                                    <th scope="col" width="25%" class="DGMuct_id">Owner ID</th>
+                                    <th scope="col" width="50%">Owner Name</th>
+                                    <th scope="col" width="25%">Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    <tr>
+                                        <td scope="row" class="DGMuct_id">DGM001</td>
+                                        <td >Fyke Lleva</td>
+                                        <td>php 100.00</td>
+                                    
+                                    </tr>
+                                    
+                                    
+                                </tbody>
+                            </table>
+                     </div>
+                </div>
+                <div class="col-12 col-md-6 p-2">
+                    <h6 class="fw-bolder">List of Solds</h6>
+                    <div class="border rounded p-2">
+                        <table class="table mt-3">
+                                    <thead>
+                                        <tr>
+                                        <th scope="col" width="25%" class="DGMuct_id">Sold ID</th>
+                                        <th scope="col" width="50%">Owner Name</th>
+                                        <th scope="col" width="25%">Sold to</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <td scope="row" class="DGMuct_id">DGM001</td>
+                                            <td >Fyke Lleva</td>
+                                            <td>Dave Bergania</td>
+                                        
+                                        </tr>
+                                        
+                                    </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+
+                
+            </div>
+            
+        </div>
+
+
+        <!-- Add Modal -->
+        <div class="modal fade mt-5" id="add_DGMuct" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Add DGMuct</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="text" class="form-control form-control-sm mb-2" placeholder="SR NO.">
+                <input type="text" class="form-control form-control-sm mb-2" placeholder="DGMuct Name">
+                <textarea name="" class="form-control form-control-sm mb-2" placeholder="Description" id="" cols="30" rows="5"></textarea>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-success">+ Add</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<!-- end inventory-->
+
+
+</div>
+<?php include 'footer.php'?>
 </body>
 </html>
