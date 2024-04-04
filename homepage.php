17b25faf0782b0f14a7ef2ec06283b4a3bcffcca
Fyke Lleva <floterina@gmail.com>
2024-04-05 01:04:26 +0800

    update footer

--- homepage.php
+++ homepage.php
@@ -31,7 +31,7 @@
                       <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg></span>  
                   <a href="" class="text-decoration-none text-light">www.facebook.com</a>
-                  </p>
+                </p>
               </div>
             </div>
 
@@ -139,9 +139,53 @@
   </div>
 
   <!-- Footer -->
-  <footer class="footer bg-dark text-light mt-5" style="height: 300px">
+  <footer class="footer bg-dark text-light mt-5 pb-5 style="height: auto">
     <div class="container">
-      <span >Place sticky footer content here.</span>
+      <div class="row pt-5">
+          <div class="col-12 col-md-4 d-flex flex-column">
+            <a href="" class="text-decoration-none text-light mb-3">Location</a>
+            <a href="" class="text-decoration-none text-light mb-3">Blog</a>
+            <a href="" class="text-decoration-none text-light mb-3">Events</a>
+            <a href="" class="text-decoration-none text-light mb-3">Services</a>
+          </div>
+          <div class="col-12 col-md-4 d-flex flex-column">
+            <a href="" class="text-decoration-none text-light mb-3">Contact</a>
+            <a href="" class="text-decoration-none text-light mb-3">About Us</a>
+            <a href="" class="text-decoration-none text-light mb-3">Privacy Policy</a> 
+            <a href="" class="text-decoration-none text-light mb-3">Terms and Condition</a>
+          </div>
+          <div class="col-12 col-md-4 d-flex flex-column">
+            <p class="text-light">
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-geo-alt-fill" viewBox="0 0 16 16">
+                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+                </svg>
+              </span>  
+              M. Villarica Rd, Marilao, Bulacan
+            </p>
+            <p class="text-light">
+                  <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-envelope-fill" viewBox="0 0 16 16">
+                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                  </svg></span>  
+                  <a href="" class="text-decoration-none text-light">divineMemorialGarden@gmail.com</a>
+            </p>
+            <p class="text-light">
+                  <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                  </svg></span>  
+                  <a href="" class="text-decoration-none text-light">0947 499 4255</a>
+                  </p>
+            <p class="text-light">
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-facebook" viewBox="0 0 16 16">
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                </svg>
+              </span>  
+              <a href="" class="text-decoration-none text-light">www.facebook.com</a>
+            </p>
+            
+          </div>
+      </div>
     </div>
   </footer>
 
