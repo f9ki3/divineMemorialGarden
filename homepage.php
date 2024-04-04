0e27366a4089ebfa05167b3614c7b03f7dabd593
Fyke Lleva <floterina@gmail.com>
2024-04-05 02:29:48 +0800

    push

--- homepage.php
+++ homepage.php
@@ -30,7 +30,7 @@
                   <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-facebook" viewBox="0 0 16 16">
                       <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg></span>  
-                  <a href="" class="text-decoration-none text-light">www.facebook.com</a>
+                  <a href="https://web.facebook.com/DivineGardenMarilaoMemorialPark/?_rdc=1&_rdr" class="text-decoration-none text-light">www.facebook.com</a>
                 </p>
               </div>
             </div>
@@ -67,127 +67,124 @@
 
   <!-- Blog post news -->
   <div class="container mt-5">
-    <h3 class="fw-bold">News and Articles</h3>
-    <div class="row">
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Blog Post Title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-success">Read more</a>
-          </div>
+  <h3 class="fw-bold">News and Articles</h3>
+  <div class="row">
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Honoring Our Heroes</h5>
+          <p class="card-text">Explore the significance of memorial sites dedicated to veterans and join us in paying tribute to those who have served.</p>
+          <a href="#" class="btn btn-success">Read more</a>
         </div>
       </div>
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Blog Post Title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-success">Read more</a>
-          </div>
+    </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Preserving History</h5>
+          <p class="card-text">Discover the importance of preserving cemeteries as vital components of our heritage and efforts underway to protect and maintain these sacred spaces.</p>
+          <a href="#" class="btn btn-success">Read more</a>
         </div>
       </div>
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Blog Post Title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-success">Read more</a>
-          </div>
+    </div>
+    <div class="col-md-4 mb-4">
+      <div class="card">
+        <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Healing Through Remembrance</h5>
+          <p class="card-text">Explore the therapeutic benefits of memorial gardens and discover how these beautiful settings help individuals find peace amidst sorrow.</p>
+          <a href="#" class="btn btn-success">Read more</a>
         </div>
       </div>
     </div>
   </div>
+</div>
+
 
   <div class="container mt-5">
     <h3 class="fw-bold">Services Offers</h3>
     <div class="row">
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Blog Post Title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-success">Apply Now</a>
-          </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div style="height: 200px;">
+                  <img src="uploads/g1.jpg" style="object-fit: cover; height: 100%; width: 100%" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Grave Maintenance</h5>
+                    <p class="card-text">Professional care and maintenance of gravesites, including cleaning, landscaping, and upkeep.</p>
+                    <a href="#" class="btn btn-success">Inquire Now</a>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Blog Post Title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-success">Apply Now</a>
-          </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div style="height: 200px;">
+                  <img src="uploads/g2.jpg" style="object-fit: cover; height: 100%; width: 100%" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Funeral Planning Services</h5>
+                    <p class="card-text">Assistance in planning and organizing funeral services, including logistics, paperwork, and ceremony arrangements.</p>
+                    <a href="#" class="btn btn-success">Inquire Now</a>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <div class="card">
-          <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Blog Post Title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-success">Apply Now</a>
-          </div>
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div style="height: 200px;">
+                  <img src="uploads/g3.jpg" style="object-fit: cover; height: 100%; width: 100%" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Memorial Design and Installation</h5>
+                    <p class="card-text">Customized memorial design and installation services, including headstones, plaques, and other commemorative structures.</p>
+                    <a href="#" class="btn btn-success">Inquire Now</a>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 
-  <!-- Footer -->
-  <footer class="footer bg-dark text-light mt-5 pb-5 style="height: auto">
-    <div class="container">
-      <div class="row pt-5">
-          <div class="col-12 col-md-4 d-flex flex-column">
-            <a href="" class="text-decoration-none text-light mb-3">Location</a>
-            <a href="" class="text-decoration-none text-light mb-3">Blog</a>
-            <a href="" class="text-decoration-none text-light mb-3">Events</a>
-            <a href="" class="text-decoration-none text-light mb-3">Services</a>
-          </div>
-          <div class="col-12 col-md-4 d-flex flex-column">
-            <a href="" class="text-decoration-none text-light mb-3">Contact</a>
-            <a href="" class="text-decoration-none text-light mb-3">About Us</a>
-            <a href="" class="text-decoration-none text-light mb-3">Privacy Policy</a> 
-            <a href="" class="text-decoration-none text-light mb-3">Terms and Condition</a>
-          </div>
-          <div class="col-12 col-md-4 d-flex flex-column">
-            <p class="text-light">
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-geo-alt-fill" viewBox="0 0 16 16">
-                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
-                </svg>
-              </span>  
-              M. Villarica Rd, Marilao, Bulacan
-            </p>
-            <p class="text-light">
-                  <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-envelope-fill" viewBox="0 0 16 16">
-                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
-                  </svg></span>  
-                  <a href="" class="text-decoration-none text-light">divineMemorialGarden@gmail.com</a>
-            </p>
-            <p class="text-light">
-                  <span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-telephone-fill" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
-                  </svg></span>  
-                  <a href="" class="text-decoration-none text-light">0947 499 4255</a>
-                  </p>
-            <p class="text-light">
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi me-3 bi-facebook" viewBox="0 0 16 16">
-                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                </svg>
-              </span>  
-              <a href="" class="text-decoration-none text-light">www.facebook.com</a>
-            </p>
-            
-          </div>
-      </div>
+
+  <div class="container mt-5">
+    
+    <div class="row mt-5">
+        <div class="col-12 col-md-6">
+        <h3 class="fw-bold mb-3">Steps to search the lot</h3>
+            <div class="step mb-3">
+                <span class="step-num">1. </span>
+                <span class="step-text">Go to the divineMemorialGarden.com</span>
+            </div>
+            <div class="step mb-3">
+                <span class="step-num">2. </span>
+                <span class="step-text">Select Map in the menu or click the picture on the right side</span>
+            </div>
+            <div class="step mb-3">
+                <span class="step-num">3. </span>
+                <span class="step-text">Select the Lawn where you want to search</span>
+            </div>
+            <div class="step mb-3">
+                <span class="step-num">4. </span>
+                <span class="step-text">Search for lot information</span>
+            </div>
+            <div class="step mb-3">
+                <span class="step-num">5. </span>
+                <span class="step-text">Select the highlighted box to view the lot information</span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <a href="map.php">
+              <div style="height: 300px; width: auto">
+                  <img src="uploads/map_highligh.png" class="rounded w-100 h-100 rounded-4" alt="map">
+              </div>
+            </a>
+            <h5 class="text-center mt-3">Divine Memorial Garden Map</h5>
+        </div>
     </div>
-  </footer>
+</div>
+
+
+<?php include 'footer.php'?>
 
   <!-- Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
