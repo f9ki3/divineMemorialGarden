215de1f8e6f49066fae01169019a76a4c85efde0
Fyke Lleva <floterina@gmail.com>
2024-04-17 07:29:17 +0800

    updated

--- client/message.php
+++ client/message.php
@@ -6,19 +6,21 @@
 
 <div id="getting_started" class="container text-secondary w-100  p-5 " >
     <h3 style="font-weight: bold;" class="mb-3">Divine Memorial Office</h3>
-    <div class="w-100 d-flex flex-row mb-3">
-            <div class="w-100 ">
-                <div class="bg-light w-50 p-3 rounded">
-                    Hello client what's your concern?
+    <div class="responsive" style="height: 450px; ">
+        <div class="w-100 d-flex flex-row mb-3">
+                <div class="w-100 ">
+                    <div class="bg-light w-50 p-3 rounded">
+                        Hello client what's your concern?
+                    </div>
                 </div>
-            </div>
-    </div>
-    <div class="w-100 d-flex flex-row mb-3">
-            <div class="w-100 ">
-                <div class="bg-success w-50 p-3 rounded text-light">
-                    Hello may I request to transfer my lot ownership.
+        </div>
+        <div class="w-100 d-flex flex-row mb-3">
+                <div class="w-100 d-flex justify-content-end">
+                    <div class="bg-success w-50 p-3 rounded text-light">
+                        Hello may I request to transfer my lot ownership.
+                    </div>
                 </div>
-            </div>
+        </div>
     </div>
     
 
