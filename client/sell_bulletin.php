9c7b460848dabd5ca9138ed7ffddb9a4f94f1d9d
Fyke Lleva <floterina@gmail.com>
2024-04-24 13:27:42 +0800

    update

--- client/sell_bulletin.php
+++ client/sell_bulletin.php
@@ -4,12 +4,16 @@
 <body>
 <?php include 'navbar.php'?>
 
-<div id="getting_started" class="row text-secondary w-100  p-5 " >
+<div class="container">
+<div id="getting_started" class="row text-secondary mt-5 w-100" >
     <div class="col-12 col-md-2">
         <h3 style="font-weight: bold;" class="mb-3">Search Filters</h3>
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search here">
-            <button class="btn btn-success">Search</button>
+            <input type="text" class="form-control" placeholder="Search">
+            <button class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                </svg>
+            </button>
         </div>
         <hr>
         <div class="mb-2">
@@ -47,7 +51,7 @@
 
     <div class="col-12 col-md-9">
         
-    <h3 style="font-weight: bold;" class="mb-3">Recently Lot Sale Posted</h3>
+            <h3 style="font-weight: bold;" class="mb-3">Recently Lot Sale Posted</h3>
             <hr>
             <div class="w-100 row text-secondary">
             <?php
@@ -65,9 +69,16 @@
                     <div class="col-12 col-md-4 p-2">
                         <div class="border rounded p-3">
                             <div>
-                                <h4 class="fw-bold text-success">₱ ' . number_format($row['bulletin_price'], 2) . '</h4>
+                                <div class="d-flex justify-content-between">
+                                    <h4 class="fw-bold text-success">₱ ' . number_format($row['bulletin_price'], 2) . '</h4>
+                                    <a href="" >
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="green" class="bi bi-cursor-fill" viewBox="0 0 16 16">
+                                            <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
+                                        </svg>
+                                    </a>
+                                </div>
                                 <hr>
-                                <h5 class="m-0 p-0">Owner: ' . $row['lot_owner'] . '</h5>
+                                <h6 class="m-0 p-0">Owner: ' . $row['lot_owner'] . '</h6>
                                 <p class="m-0 p-0">Lot: Block ' . $row['block_number'] . ' Lot ' . $row['lot_number'] . '</p>
                                 <p class="m-0 p-0">Contact: ' . $row['bulletin_contact'] . '</p>
                                 <p class="m-0 p-0">Email: ' . $row['bulletin_email'] . '</p>
@@ -75,7 +86,7 @@
                         </div>    
                     </div>
                 ';        
-                }
+                } 
             } else {
                 echo '
                 <div style="height: 70vh; display: flex; flex-direction: column; justify-content: center; align-items: center" class="">
@@ -104,6 +115,7 @@
             </div>
     </div>
     
+</div>
 </div>
 
     <?php include ('footer.php')?>
