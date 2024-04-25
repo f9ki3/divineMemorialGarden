6b7ba30371ce473646f56567cd9f3bf54fe74e75
Fyke Lleva <floterina@gmail.com>
2024-04-25 14:30:07 +0800

    updated

--- client/message.php
+++ client/message.php
@@ -25,7 +25,7 @@ overflow: hidden;
         </div>
         <div class="col-12 col-md-9 h-100 d-flex flex-column justify-content-between p-3">
 
-            <h3 style="font-weight: bold;" class="mb-3">Divine Memorial Office</h3>
+            <h3 style="font-weight: bold;" class="mb-0" id='user'></h3>
     <hr>
             <div class="responsive overflow-auto" style="height: calc(100% - 60px);"><!-- Adjusted height -->
                 <div id="message_open"></div>
@@ -38,7 +38,7 @@ overflow: hidden;
                 <input value="<?php echo $id;?>" type="hidden" class="form-control mb-2" type="text" id="sender_id" placeholder="Sender ID">
                 <input id="message_content" type="text" class="form-control me-2 " placeholder="Write Message" style="height: 50px">
                 <div class="d-flex flex-row" style="width: 30%">
-                    <button class="btn btn-light border w-50" onclick="send_message()" style="height: 50px">
+                    <button id="send_message" class="btn btn-light border w-50" onclick="send_message()" style="height: 50px">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cursor-fill" viewBox="0 0 16 16">
                     <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
                     </svg>
@@ -88,10 +88,10 @@ $(document).ready(function() {
 
     const maxLength = 60;
     const truncatedMessage = truncateMessage(message.message_content, maxLength);
-
+    
     // Construct HTML for each message
     var messageHtml = `
-        <a class="view_message text-decoration-none text-muted" data-receiver-id="${message.reciever_id}">
+        <a class="view_message text-decoration-none text-muted" data-receiver-id="${message.reciever_id}" data-user_fname="${message.user_fname}" data-user_lname="${message.user_lname}">
             <div class="message rounded border p-2 mt-2 d-flex flex-row">
                 <img src="../uploads/${message.user_profile}" style="height: 50px; width: 50px; border-radius: 100%;" alt="">
                 <div class="ms-3 d-flex flex-column">
@@ -119,13 +119,21 @@ $(document).ready(function() {
     // Set interval to reload messages every 30 seconds (adjust as needed)
     setInterval(function() {
         loadMessages(); // Reload messages
-    }, 30000); // 30 seconds interval (30000 milliseconds)
+    }, 3000); // 30 seconds interval (30000 milliseconds)
 
     // Function to handle click on message
     function handleMessageClick() {
         $(document).on('click', '.view_message', function() {
             var receiverId = $(this).data('receiver-id'); // Get the receiver_id from data attribute
+            var user_fname = $(this).data('user_fname'); 
+            var user_lname = $(this).data('user_lname'); 
+            var fullname =user_fname + ' ' + user_lname;
 
+            $('#user').text(fullname);
+            
+            // Set the value of the element with id="received_id"
+            $('#recieved_id').val(receiverId);
+            console.log(receiverId)
             // Make an Ajax request to message_open.php
             $.ajax({
                 url: '../php/message_open.php',
@@ -154,7 +162,7 @@ $(document).ready(function() {
                             var messageHtml = `
                                 <div class=" d-flex flex-row mb-3">
                                     <div class="w-100 ${align}">
-                                        <div class="rounded-5 p-3 border rounded  ${messageAlignmentClass}">
+                                        <div style="max-width: 50%" class="rounded-5 p-3 border rounded  ${messageAlignmentClass}">
                                         ${message.message_content}
                                         </div>
                                     </div>
@@ -182,10 +190,13 @@ $(document).ready(function() {
     // Call the handleMessageClick function to set up click event handling
     handleMessageClick();
 
-    function send_message() {
+});
+
+function send_message() {
         var recieved_id = $('#recieved_id').val();
         var sender_id = $('#sender_id').val();
         var message_content = $('#message_content').val().trim();
+        $('#message_content').val('');
 
         // Validate message content
         if (message_content === '') {
@@ -228,6 +239,7 @@ $(document).ready(function() {
             modalInstance.hide();
             alertify.set('notifier','position', 'bottom-left');
             alertify.success('Message Sent!');
+            handleMessageClick()
 
             var messageContentElement = document.getElementById('message_content');
                 if (messageContentElement) {
@@ -252,7 +264,6 @@ $(document).ready(function() {
         var charactersLeft = maxCharacters - messageLength;
         $('#charCount').text("Characters left: " + charactersLeft);
         });
-});
 
 
 </script>
